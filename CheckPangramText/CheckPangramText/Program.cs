0b14b1b23937d8b11479264ec9c441c982629b46
Trushit777 <52493492+Trushit777@users.noreply.github.com>
2020-10-11 11:08:56 +0530

    Update Program.cs
    Code Optimized(Using Switch case is taking much time so replaced the code)

--- CheckPangramText/CheckPangramText/Program.cs
+++ CheckPangramText/CheckPangramText/Program.cs
@@ -2,109 +2,51 @@
 
 namespace CheckPangramText
 {
-    class Program
+   class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(CheckPangram("we promptly judge antique ivory buckles for the next prize"));
+            ComeHere:
+            Console.Write("\nEnter Text for Checking Pangram : ");
+            string data = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                Console.WriteLine(CheckPangram(data));
+                goto ComeHere;
+            }
+            else
+            {
+                Console.WriteLine(" \nEnter Text First");
+                goto ComeHere;
+            }
+
         }
 
         public static string CheckPangram(string text)
         {
             try
             {
-                int count = 0;
-                var data = new string(text.Distinct().ToArray());
-                foreach (var item in data)
+                string pengram = "not pangram";
+                string p = new string(text.Distinct().ToArray());
+                int t = 0;
+                string[] a = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".Split(",").ToArray();
+                for (int i = 0; i < 26; i++)
                 {
-                    switch (item)
+                    if (p.Contains(a[i]))
                     {
-                        case 'a':
-                            count = count + 1;
-                            break;
-                        case 'b':
-                            count = count + 1;
-                            break;
-                        case 'c':
-                            count = count + 1;
-                            break;
-                        case 'd':
-                            count = count + 1;
-                            break;
-                        case 'e':
-                            count = count + 1;
-                            break;
-                        case 'f':
-                            count = count + 1;
-                            break;
-                        case 'g':
-                            count = count + 1;
-                            break;
-                        case 'h':
-                            count = count + 1;
-                            break;
-                        case 'i':
-                            count = count + 1;
-                            break;
-                        case 'j':
-                            count = count + 1;
-                            break;
-                        case 'k':
-                            count = count + 1;
-                            break;
-                        case 'l':
-                            count = count + 1;
-                            break;
-                        case 'm':
-                            count = count + 1;
-                            break;
-                        case 'n':
-                            count = count + 1;
-                            break;
-                        case 'o':
-                            count = count + 1;
-                            break;
-                        case 'p':
-                            count = count + 1;
-                            break;
-                        case 'q':
-                            count = count + 1;
-                            break;
-                        case 'r':
-                            count = count + 1;
-                            break;
-                        case 's':
-                            count = count + 1;
-                            break;
-                        case 't':
-                            count = count + 1;
-                            break;
-                        case 'u':
-                            count = count + 1;
-                            break;
-                        case 'v':
-                            count = count + 1;
-                            break;
-                        case 'w':
-                            count = count + 1;
-                            break;
-                        case 'x':
-                            count = count + 1;
-                            break;
-                        case 'y':
-                            count = count + 1;
-                            break;
-                        case 'z':
-                            count = count + 1;
-                            break;
-                        default:
-                            break;
+                        t++;
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
-                if (count == 26)
-                    return "pangram";
-                else
-                    return "not pangram";
+                if (t == 26)
+                {
+                    pengram = "pangram";
+                }
+                return pengram;
             }
             catch (Exception ex)
             {
